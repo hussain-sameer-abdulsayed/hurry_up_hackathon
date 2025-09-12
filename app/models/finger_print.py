9b3faa091dda8aab22137e29de601e684c9df542
Hussain <hussainsameer1718@gmail.com>
2025-09-12 23:37:37 +0300

    fix errors names

--- app/models/finger_print.py
+++ app/models/finger_print.py
@@ -1,13 +1,14 @@
 from datetime import datetime
-from uuid import UUID
+from uuid import UUID, uuid4
 from sqlmodel import Field, SQLModel
 
 
 class FingerPrint(SQLModel, table=True):
-   __tablename__ = "fingerprints"
-   id: UUID = Field(default_factory= None, primary_key= True)
-   template_data: str
-   minutiae_count: int
-   quality_score: float
-   created_at: datetime = datetime.utcnow()
-
+    __tablename__ = "fingerprints"
+    
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    template_data: str
+    minutiae_count: int
+    quality_score: float
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    filename: str | None = None
--- app/repos/fingerprint_repo.py
+++ app/repos/fingerprint_repo.py
@@ -6,20 +6,17 @@ from app.schemas.finger_print import FingerPrintCreate
 
 
 class FingerPrintRepository:
-   def __init__(self, db:AsyncSession):
-      self.db = db
+    def __init__(self, db: AsyncSession):
+        self.db = db
 
-
-   async def create(self, data: FingerPrintCreate):
-      fingerprint = FingerPrint(**data.dict())
-      self.db.add(fingerprint)
-      await self.db.commit()
-      await self.db.refresh(fingerprint)
-      return fingerprint
-   
-   async def get_all(self):
-      statement = select(FingerPrint).order_by(desc(FingerPrint.created_at()))
-      result = await self.db.execute(statement)
-      return list(result.scalars().all())
-   
-   
+    async def create(self, data: FingerPrintCreate):
+        fingerprint = FingerPrint(**data.model_dump())
+        self.db.add(fingerprint)
+        await self.db.commit()
+        await self.db.refresh(fingerprint)
+        return fingerprint
+    
+    async def get_all(self):
+        statement = select(FingerPrint).order_by(desc(FingerPrint.created_at))
+        result = await self.db.execute(statement)
+        return list(result.scalars().all())
